f0c1de3ba9c680060d27cd8fc5d3227427f0fd4e
vaishnavibankar1405 <vaishnavibankar50@gmail.com>
2025-05-01 17:52:27 +0530

    Update QN.py

--- QN.py
+++ QN.py
@@ -31,3 +31,39 @@ def print_board(board, n):
 # Change n to any number >= 4
 n = 6
 solve_n_queens(n)
+
+
+"""
+def is_safe(board, row, col, n):
+    for i in range(row):
+        # Check same column, left diagonal, right diagonal
+        if board[i] == col or board[i] - i == col - row or board[i] + i == col + row:
+            return False
+    return True
+
+def solve(row, board, n):
+    if row == n:
+        print_sol(board, n)
+        return True
+
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row] = col
+            if solve(row + 1, board, n):
+                return True
+
+    return False
+
+def print_sol(board, n):
+    for i in range(n):
+        row = ['.'] * n
+        row[board[i]] = 'Q'
+        print(" ".join(row))
+
+print("using backtracking")
+n = int(input("Enter N: "))
+board = [-1] * n
+
+if not solve(0, board, n):
+    print("No solution found")
+"""
